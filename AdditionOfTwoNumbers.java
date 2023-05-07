94d79fa08182a16fe7bae7a76d3afd94d1dfe30b
BuildTools <unconfigured@null.spigotmc.org>
2023-05-07 14:08:10 +0530

    Added code for addition of two numbers!

---
+++ AdditionOfTwoNumbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class add{
+public static void main(String[] args){
+Scanner s = new Scanner(System.in);
+long a= s.nextLong();
+long b=s.nextLong();
+long c= a+b;
+System.out.println(c);
+   }   
+}
